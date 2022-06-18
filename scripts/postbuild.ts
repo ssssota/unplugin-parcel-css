4b61751c07e6d4c2b4e6f67078f3d67b73f15f63
ssssota <tommysouta@gmail.com>
2022-06-18 14:50:37 +0900

    fix: postbuild script for commonjs default export

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -12,13 +12,18 @@ async function run() {
   for (const file of files) {
     // eslint-disable-next-line no-console
     console.log(chalk.cyan.inverse(' POST '), `Fix ${basename(file)}`);
-    if (file === 'index.js') {
-      // fix cjs exports
-      let code = await fs.readFile(file, 'utf8');
-      code = code.replace('exports.default =', 'module.exports =');
-      code += 'exports.default = module.exports;';
-      await fs.writeFile(file, code);
-    }
+
+    let code = await fs.readFile(file, 'utf8');
+    code = code.replace(
+      'var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);',
+      'var __toCommonJS = (mod) => __copyProps(__defProp(mod.default ?? {}, "__esModule", { value: true }), mod);'
+    );
+    const moduleExports = code.match(/^module.exports = .*$/m)?.[0];
+    if (moduleExports === undefined)
+      throw new Error('module.exports is not defined');
+    code += moduleExports;
+    await fs.writeFile(file, code);
+
     // generate submodule .d.ts redirecting
     const name = basename(file, '.js');
     await fs.writeFile(
